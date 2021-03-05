0e2ff8dc1009dccab4d2e17ed53ba65c4e99450e
planet36 <planet36@users.noreply.github.com>
2021-03-05 14:20:29 -0500

    verr: Remove special "usage" case
    In function verr, remove special case for "usage"
    string
    
    Co-authored-by: drkhsh <me@drkhsh.at>
    Signed-off-by: drkhsh <me@drkhsh.at>

--- util.c
+++ util.c
@@ -13,9 +13,6 @@ char *argv0;
 static void
 verr(const char *fmt, va_list ap)
 {
-	if (argv0 && strncmp(fmt, "usage", sizeof("usage") - 1))
-		fprintf(stderr, "%s: ", argv0);
-
 	vfprintf(stderr, fmt, ap);
 
 	if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
