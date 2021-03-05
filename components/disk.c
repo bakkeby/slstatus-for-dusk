89f8476110c7bdfb70528da79be328ba0f6490be
planet36 <planet36@users.noreply.github.com>
2021-03-05 14:28:15 -0500

    disk: Cast fsblkcnt_t to double instead of float
    Signed-off-by: drkhsh <me@drkhsh.at>

--- components/disk.c
+++ components/disk.c
@@ -29,7 +29,7 @@ disk_perc(const char *path)
 	}
 
 	return bprintf("%d", (int)(100 *
-	               (1.0f - ((float)fs.f_bavail / (float)fs.f_blocks))));
+	               (1 - ((double)fs.f_bavail / (double)fs.f_blocks))));
 }
 
 const char *
