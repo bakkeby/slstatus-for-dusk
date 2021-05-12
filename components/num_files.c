984f45719e8ac9f4451c2d009fb34e28afdfbdb6
planet36 <planet36@users.noreply.github.com>
2021-05-11 22:45:34 -0400

    num_files: opendir() returns a directory stream
    opendir() returns a directory stream, not a file descriptor
    
    Co-authored-by: drkhsh <me@drkhsh.at>
    Signed-off-by: drkhsh <me@drkhsh.at>

--- components/num_files.c
+++ components/num_files.c
@@ -10,23 +10,23 @@ const char *
 num_files(const char *path)
 {
 	struct dirent *dp;
-	DIR *fd;
+	DIR *dir;
 	int num;
 
-	if (!(fd = opendir(path))) {
+	if (!(dir = opendir(path))) {
 		warn("opendir '%s':", path);
 		return NULL;
 	}
 
 	num = 0;
-	while ((dp = readdir(fd))) {
+	while ((dp = readdir(dir))) {
 		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
 			continue; /* skip self and parent */
 
 		num++;
 	}
 
-	closedir(fd);
+	closedir(dir);
 
 	return bprintf("%d", num);
 }
