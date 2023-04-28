60463777e0086db0cdd23883cdaeff9b70f635d9
Bakkeby <bakkeby@gmail.com>
2023-04-28 11:30:12 +0200

    Refactoring ARGBEGIN and ARGEND macros for readability

--- arg.h
+++ arg.h
@@ -5,25 +5,29 @@
 extern char *argv0;
 
 /* int main(int argc, char *argv[]) */
-#define ARGBEGIN for (argv0 = *argv, *argv ? (argc--, argv++) : ((void *)0);      \
-                      *argv && (*argv)[0] == '-' && (*argv)[1]; argc--, argv++) { \
-                 	int i_, argused_;                                         \
-                 	if ((*argv)[1] == '-' && !(*argv)[2]) {                   \
-                 		argc--, argv++;                                   \
-                 		break;                                            \
-                 	}                                                         \
-                 	for (i_ = 1, argused_ = 0; (*argv)[i_]; i_++) {           \
-				switch ((*argv)[i_])
-#define ARGEND   		if (argused_) {                                   \
-                 			if ((*argv)[i_ + 1]) {                    \
-                 				break;                            \
-                 			} else {                                  \
-                 				argc--, argv++;                   \
-                 				break;                            \
-                 			}                                         \
-                 		}                                                 \
-                 	}                                                         \
-                 }
+#define ARGBEGIN \
+    for (argv0 = *argv, *argv ? (argc--, argv++) : ((void *)0);                   \
+        *argv && (*argv)[0] == '-' && (*argv)[1]; argc--, argv++)                 \
+    {                                                                             \
+        int i_, argused_;                                                         \
+        if ((*argv)[1] == '-' && !(*argv)[2]) {                                   \
+            argc--, argv++;                                                       \
+            break;                                                                \
+        }                                                                         \
+        for (i_ = 1, argused_ = 0; (*argv)[i_]; i_++) {                           \
+            switch ((*argv)[i_]) {
+#define ARGEND \
+                if (argused_) {                                                   \
+                    if ((*argv)[i_ + 1]) {                                        \
+                        break;                                                    \
+                    } else {                                                      \
+                        argc--, argv++;                                           \
+                        break;                                                    \
+                    }                                                             \
+                }                                                                 \
+            }                                                                     \
+        }                                                                         \
+    }
 #define ARGC()    ((*argv)[i_])
 #define ARGF_(x) (((*argv)[i_ + 1]) ? (argused_ = 1, &((*argv)[i_ + 1])) :        \
                   (*(argv + 1))     ? (argused_ = 1, *(argv + 1))        : (x))
