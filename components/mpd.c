34136878c0c08f618d6c0f73d27a2c9adeb8a944
David Armstrong <vide0hanz@protonmail.com>
2025-01-13 01:11:59 -0600

    Song titles over 24 characters long will scroll left by 1 letter per update interval

--- components/mpd.c
+++ components/mpd.c
@@ -14,6 +14,44 @@
  * i.e: "a-t" gives "artist-title"
 */
 
+#define TITLE_LENGTH 24
+
+void scroll_title(const char *title, enum mpd_state state) {
+  char scroll[TITLE_LENGTH + 1] = {'\0'};
+  static char prev[256] = {'\0'};
+  static int idx = 0;
+
+  if (title && strcmp(title, prev) != 0) {
+    idx = 0;
+    strncpy(prev, title, sizeof(prev) - 1);
+    prev[sizeof(prev) - 1] = '\0';
+  }
+
+  if (state == MPD_STATE_PLAY || state == MPD_STATE_PAUSE) {
+    if (strlen(title) > TITLE_LENGTH) {
+      snprintf(scroll, TITLE_LENGTH + 1, "%s", title + idx);
+      strncat(buf, scroll, sizeof(buf) - strlen(buf) - 1);
+      if (state == MPD_STATE_PLAY) {
+        idx++;
+        if ((size_t)(idx) > strlen(title) - TITLE_LENGTH) {
+          idx = 0;
+        }
+      }
+    } else {
+      strncat(buf, title, sizeof(buf) - strlen(buf) - 1);
+    }
+
+  } else if (state == MPD_STATE_STOP) {
+    idx = 0;
+    if (strlen(title) > TITLE_LENGTH) {
+      snprintf(scroll, TITLE_LENGTH + 1, "%s", title);
+      strncat(buf, scroll, sizeof(buf) - strlen(buf) - 1);
+    } else {
+      strncat(buf, title, sizeof(buf) - strlen(buf) - 1);
+    }
+  }
+}
+
 const char *
 mpdonair(const char *fmt) {
   static struct mpd_connection *conn; /* kept between calls */
@@ -30,21 +68,7 @@ mpdonair(const char *fmt) {
 
   mpd_send_status(conn);
   status = mpd_recv_status(conn);
-
   if (status == NULL) {
-    goto mpdout;
-  }
-
-  enum mpd_state state = mpd_status_get_state(status);
-  buf[0] = '\0';
-
-  if (state == MPD_STATE_PLAY) {
-    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
-  } else if (state == MPD_STATE_PAUSE) {
-    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
-  } else if (state == MPD_STATE_STOP) {
-    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
-  } else if (state == MPD_STATE_UNKNOWN) {
     goto mpdout;
   }
 
@@ -53,22 +77,39 @@ mpdonair(const char *fmt) {
   if (song == NULL) {
     goto mpdout;
   } else {
+    buf[0] = '\0';
+    enum mpd_state state = mpd_status_get_state(status);
+    if (state == MPD_STATE_PLAY) {
+      strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+    } else if (state == MPD_STATE_PAUSE) {
+      strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+    } else if (state == MPD_STATE_STOP) {
+      strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+    } else if (state == MPD_STATE_UNKNOWN) {
+      goto mpdout;
+    }
+
+    const char *title = mpd_song_get_tag(song, MPD_TAG_TITLE, 0);
+    const char *artist = mpd_song_get_tag(song, MPD_TAG_ARTIST, 0);
 
     for (int i = 0; i < (int)strlen(fmt); i++) {
       char separator[2] = {fmt[i], '\0'};
       switch (fmt[i]) {
         case 'a':
-          strncat(buf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0), sizeof(buf) - strlen(buf) - 1);
+          if (artist != NULL) {
+            strncat(buf, artist, sizeof(buf) - strlen(buf) - 1);
+          }
           break;
         case 't':
-          strncat(buf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0), sizeof(buf) - strlen(buf) - 1);
+          if (title != NULL) {
+            scroll_title(title, state);
+          }
           break;
         default:
           strncat(buf, separator, sizeof(buf) - strlen(buf) - 1);
           break;
       }
     }
-
     mpd_status_free(status);
     mpd_song_free(song);
     mpd_response_finish(conn);
