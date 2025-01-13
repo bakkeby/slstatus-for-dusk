8d51675fcfe8caad86a75b7883eec2a7af7f8a15
David Armstrong <vide0hanz@protonmail.com>
2025-01-12 19:09:27 -0600

    Added icons for play, pause, and stop states; removed unused declarations

---
+++ components/mpd.c
@@ -0,0 +1,83 @@
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <mpd/client.h>
+
+#include "../util.h"
+
+/* fmt consist of lowercase :
+ * "a" for artist,
+ * "t" for song title,
+ * "at" for song artist then title
+ * if not a or t, any character will be represented as separator.
+ * i.e: "a-t" gives "artist-title"
+*/
+
+const char *
+mpdonair(const char *fmt) {
+  static struct mpd_connection *conn; /* kept between calls */
+  struct mpd_song *song = NULL;
+  struct mpd_status *status = NULL;
+
+  if (conn == NULL) {
+    conn = mpd_connection_new(NULL, 0, 5000);
+    if (conn == NULL) {
+      warn("MPD error: %s", mpd_connection_get_error_message(conn));
+      goto mpdout;
+    }
+  }
+
+  mpd_send_status(conn);
+  status = mpd_recv_status(conn);
+
+  if (status == NULL) {
+    goto mpdout;
+  }
+
+  enum mpd_state state = mpd_status_get_state(status);
+  buf[0] = '\0';
+
+  if (state == MPD_STATE_PLAY) {
+    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+  } else if (state == MPD_STATE_PAUSE) {
+    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+  } else if (state == MPD_STATE_STOP) {
+    strncat(buf, "  ", sizeof(buf) - strlen(buf) -1);
+  } else if (state == MPD_STATE_UNKNOWN) {
+    goto mpdout;
+  }
+
+  mpd_send_current_song(conn);
+  song = mpd_recv_song(conn);
+  if (song == NULL) {
+    goto mpdout;
+  } else {
+
+    for (int i = 0; i < (int)strlen(fmt); i++) {
+      char separator[2] = {fmt[i], '\0'};
+      switch (fmt[i]) {
+        case 'a':
+          strncat(buf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0), sizeof(buf) - strlen(buf) - 1);
+          break;
+        case 't':
+          strncat(buf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0), sizeof(buf) - strlen(buf) - 1);
+          break;
+        default:
+          strncat(buf, separator, sizeof(buf) - strlen(buf) - 1);
+          break;
+      }
+    }
+
+    mpd_status_free(status);
+    mpd_song_free(song);
+    mpd_response_finish(conn);
+    return(buf);
+  }
+
+mpdout:
+  mpd_response_finish(conn);
+  mpd_connection_free(conn);
+  conn = NULL;
+  return "";
+}
